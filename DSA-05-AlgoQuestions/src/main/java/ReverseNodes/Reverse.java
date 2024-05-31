6c38c22964862b182a76d6645dec3cf5f8e0a6b1
omrzcn <omrzcnn@gmail.com>
2024-06-01 00:10:33 +0300

    questions

--- DSA-05-AlgoQuestions/src/main/java/ReverseNodes/Reverse.java
+++ DSA-05-AlgoQuestions/src/main/java/ReverseNodes/Reverse.java
@@ -3,11 +3,17 @@ package ReverseNodes;
 public class Reverse {
     public static void main(String[] args) {
         MySinglyLinkedList mySinglyLinkedList = new MySinglyLinkedList();
-
+        for (int i = 0; i < 10; i++) {
+            mySinglyLinkedList.add(i);
+        }
+        mySinglyLinkedList.printNodes();
+        reverse(mySinglyLinkedList);
+        mySinglyLinkedList.printNodes();
 
     }
 
     public static void reverse (MySinglyLinkedList list){
+        // i just need 3 pointers in turn prev-->current-->nextNode
         // i created 2 pointers
         Node prev = list.head;
         Node current = list.head.next;
