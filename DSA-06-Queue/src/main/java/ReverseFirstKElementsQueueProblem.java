75b3fa82a8b0e898aee54dd0038b1b827fe10d7c
omrzcn <omrzcnn@gmail.com>
2024-06-02 17:57:06 +0300

    stack and Question

--- DSA-06-Queue/src/main/java/ReverseFirstKElementsQueueProblem.java
+++ DSA-06-Queue/src/main/java/ReverseFirstKElementsQueueProblem.java
@@ -17,7 +17,42 @@ public class ReverseFirstKElementsQueueProblem {
 
     }
 
-    public  static MyQueue<Integer> reverseFirstK(MyQueue<Integer> queue, int k){ // 5-4-3-2-1
+  public static MyQueue<Integer> reverseFirstK(MyQueue<Integer>myQueue,int k){ // lets say myQueue has 5,4,3,2,1
+
+        Stack<Integer> stack = new Stack<>(); // stack created for accepting elements with reversed order
+
+        // lets add queue elements to our stack
+
+      for (int i = 0; i < k; i++) {
+          stack.push(myQueue.dequeue()); // stack becomes 1,2,3,4,5
+      }
+
+      while (!stack.isEmpty()){
+          myQueue.enqueue(stack.pop()); // myQueue becomes 5,4,3,2,1
+
+      }
+
+      for (int i = 0; i < myQueue.size-k; i++) {
+          myQueue.enqueue(myQueue.dequeue()); // last myqueue becomes 1,2,3,4,5
+      }
+
+
+        return myQueue;
+  }
+
+
+}
+/*
+Given an integer K and a queue of integers, write code to reverse the order
+of the first K elements of the queue.
+-Input: Q = [10, 20, 30, 40, 50], K = 4
+-Output: Q = [40,30, 20, 10,50]
+
+Hint: You can use stack to reverse the order of first K elements
+ */
+
+/*
+  public  static MyQueue<Integer> reverseFirstK(MyQueue<Integer> queue, int k){ // 5-4-3-2-1
 
         Stack<Integer> stack = new Stack<>();
 
@@ -34,14 +69,4 @@ public class ReverseFirstKElementsQueueProblem {
         }
         return queue;
     }
-
-
-}
-/*
-Given an integer K and a queue of integers, write code to reverse the order
-of the first K elements of the queue.
--Input: Q = [10, 20, 30, 40, 50], K = 4
--Output: Q = [40,30, 20, 10,50]
-
-Hint: You can use stack to reverse the order of first K elements
  */
